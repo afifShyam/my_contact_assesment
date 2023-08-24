58a036796423d4ae9abf976b024042fd31475ca4
afifShyam <114848846+afifShyam@users.noreply.github.com>
2023-08-24 15:43:57 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\afifa\\OneDrive - Universiti Teknologi MARA\\Documents\\Latihan Industri\\my_contact" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\afifa\\OneDrive - Universiti Teknologi MARA\\Documents\\Latihan Industri\\my_contact"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\afifa\\OneDrive - Universiti Teknologi MARA\\Documents\\Latihan Industri\\my_contact\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\afifa\\OneDrive - Universiti Teknologi MARA\\Documents\\Latihan Industri\\my_contact"
+  "FLUTTER_TARGET=C:\\Users\\afifa\\OneDrive - Universiti Teknologi MARA\\Documents\\Latihan Industri\\my_contact\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC80NWY2ZTAwOTExMGRmNGYzNGVjMmNmOTlmNjNjZjczYjcxYjdhNDIwLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\afifa\\OneDrive - Universiti Teknologi MARA\\Documents\\Latihan Industri\\my_contact\\.dart_tool\\package_config.json"
+)
